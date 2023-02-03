c7e1adb77171d5ece782e02013fc1d3ff67af4ef
kleciaf <kel.farias@hotmail.com>
2023-02-03 18:18:35 -0300

    Criando ações

--- app/controllers/veterinarios_controller.rb
+++ app/controllers/veterinarios_controller.rb
@@ -1,2 +1,36 @@
-class VeterinariosController < ApplicationController
+class VeterinariosController < ApplicationControlle 
+    def index 
+      @veterinarios = veterinario.all     
+    end
+    
+    def show 
+      @veterinario = veterinario.find(params[:id])  
+    end
+
+    def new
+      @veterinario = veterinario.new 
+    end
+
+    def edit
+      @veterinario = veterinario.find(params[:id]) 
+    end
+
+    def create 
+      @veterinario = veterinario.new(veterinario_params[:id])
+      if @veterinario.save
+        redirect_to @veterinario, notice: 'Veterinario criado com sucesso.'
+        
+      else
+
+        render :new
+      end  
+        
+    end  
+        
+    
+    private 
+      def veterinario_params
+        params.require(:veterinario).permit(:nome, :cmrv, :especialidades)
+      end
+    
 end
